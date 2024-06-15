c433e5990001c20ef12142f0178d0475bd1f5d37
kaungsett <rrocket438@gmail.com>
2024-06-15 16:18:32 +0630

    menu bar responsive

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+    document.addEventListener('DOMContentLoaded', function() {
+        document.querySelector('.menu').addEventListener('click', function() {
+            let menuItems = document.querySelectorAll('.menu-list-none');
+            menuItems.forEach(function(item) {
+                if (item.style.display === 'none' || item.style.display === '') {
+                    item.style.display = 'block';
+                } else {
+                    item.style.display = 'none';
+                }
+            });
+        });
+    });
