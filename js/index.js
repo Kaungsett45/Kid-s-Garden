000fee828b24c66f966c7fe383e4c657c26d8954
kaungsett <rrocket438@gmail.com>
2024-06-16 11:31:18 +0630

    responsive nav bar

--- js/index.js
+++ js/index.js
@@ -11,3 +11,19 @@
             });
         });
     });
+    document.addEventListener('DOMContentLoaded', function() {
+        document.querySelector('.menu').addEventListener('click', function() {
+            let menuItems = document.querySelectorAll('.menu-list-one');
+            menuItems.forEach(function(item) {
+                if (item.style.display === 'none' || item.style.display === '') {
+                    item.style.display = 'block';
+                } else {
+                    item.style.display = 'none';
+                }
+            });
+        });
+    });
+
+
+
+   
